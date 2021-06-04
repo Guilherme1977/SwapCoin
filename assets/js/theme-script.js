d83caa777c68f97124fe7a6a60e929fdc2066dc1
Jay B <79182784+JayBee-K@users.noreply.github.com>
2021-06-04 16:56:09 +0700

    Check

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -24,5 +24,39 @@ $(function () {
 				spaceBetween: 30,
 			}
 		}
-	})
+	});
+	
+	$("#watch-video").fancybox();
+	
+	$('.call-dropdownCoin').click(function () {
+		callDropdownCoin($(this));
+	});
+	
+	$(document).on("mouseup", function (e) {
+		var o = $(".dropdown-coin.show");
+		o.is(e.target) || 0 !== o.has(e.target).length
+		|| (o.removeClass('show'))
+	});
+	
+	$('.chooseCoin > ul > li > a').click(function () {
+		changeCoin($(this));
+	});
 });
+
+const callDropdownCoin = function (elm) {
+	let dropdown_coinElm = elm.closest('.group-item').find('.dropdown-coin');
+	if (dropdown_coinElm.hasClass('show')) {
+		dropdown_coinElm.removeClass('show');
+	} else {
+		$('.template-7_banner .group-item .dropdown-coin').removeClass('show');
+		dropdown_coinElm.addClass('show');
+	}
+}
+var changeCoin = function (elm) {
+	let img = elm.data('image'),
+		name = elm.data('name');
+	
+	elm.parents('#form-coin .group-item').find('.call-dropdownCoin img').prop('src', img);
+	elm.parents('#form-coin .group-item').find('.call-dropdownCoin .title').text(name);
+	elm.parents('#form-coin .dropdown-coin').removeClass('show');
+}
